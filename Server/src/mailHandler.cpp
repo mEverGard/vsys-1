851bbb8d40f34ad28ce3c15c22b727c628dca9ec
Alex_ <a.gamer@live.at>
2020-11-25 14:30:52 +0100

    basic handler done

--- Server/src/mailHandler.cpp
+++ Server/src/mailHandler.cpp
@@ -8,15 +8,15 @@
 
 void mailHandler(char *input)
 {
-    printf("MMessage received: %s", input);
+    // printf("MMessage received: %s", input);
     std::vector<std::string> messageParsed;
     std::string parsed;
     std::string temp(input);
     std::stringstream strm(temp);
-    std::getline(strm, parsed);
     while (std::getline(strm, parsed))
     {
         messageParsed.push_back(parsed);
     }
-    // std::cout << parsed << std::endl;
+    std::cout << "METHOD: " << messageParsed[0] << std::endl;
+    // TODO: ROUTING HERE
 }
