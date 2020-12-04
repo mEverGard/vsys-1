03e89a016cdcce70816a553d017667c88450f97c
„JorgeFHT“ <if19b103@technikum-wien.at>
2020-12-04 01:50:08 -0800

    Hotfix LIST/SEND

--- Server/src/mailHandler.cpp
+++ Server/src/mailHandler.cpp
@@ -13,25 +13,14 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <filesystem>
-<<<<<<< HEAD
-=======
 #include <algorithm>
 #include <cstdio>
->>>>>>> ec6f38af7bbbfb294cb6ed25bddd8fa5073bc915
 
 void defaultMethod(std::vector<std::string> messageParsed)
 {
     std::cout << "METHOD: " << messageParsed[0] << std::endl;
 }
 
-<<<<<<< HEAD
-void returnMessage(int socket, const char *message)
-{
-
-    send(socket, message, strlen(message), 0);
-}
-
-=======
 void returnMessage(int socket, char *message)
 {
     send(socket, message, strlen(message), 0);
@@ -39,26 +28,25 @@ void returnMessage(int socket, char *message)
 
 int checkName (std::string fileName, std::string msgnumber){
     
-    if (fileName != "." && fileName != ".."  && fileName != "count"){
+    if (fileName != "." && fileName != ".."  && fileName != "index"){
         size_t found = fileName.find(msgnumber);
         if (found != std::string::npos) return 0; //Create a procedure in case +9 emails. For example, number at begin
     }
     return 1;
 }
 
->>>>>>> ec6f38af7bbbfb294cb6ed25bddd8fa5073bc915
 int getCount(std::string path)
 {
     int i = 1;
     std::string temp;
-    std::ifstream outfile(path + "/count");
+    std::ifstream outfile(path + "/index");
     if (outfile.is_open())
     {
         getline(outfile, temp);
         i = std::stoi(temp) + 1;
         outfile.close();
     }
-    std::ofstream infile(path + "/count");
+    std::ofstream infile(path + "/index");
     if (infile.is_open())
     {
         infile << i << "\n";
@@ -66,35 +54,25 @@ int getCount(std::string path)
     return i;
 }
 
-<<<<<<< HEAD
-const char *sendHandler(std::vector<std::string> message, char *dir)
-=======
-char *sendHandler(std::vector<std::string> message, char *dir)
->>>>>>> ec6f38af7bbbfb294cb6ed25bddd8fa5073bc915
+void sendHandler(std::vector<std::string> message, char *dir, int soc)
 {
     // METHOD USERNAME RECEIVER SUBJECT MESSAGE
     std::string path = dir;
     path += '/' + message[2];
-
+    std::string out = "";
+    
     if (opendir(path.c_str()) == NULL)
     {
         // read/write/search permissions for owner and group, and with read/search permissions for others
         if (mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0)
         {
             perror("Failed to open/create user directory.\n.");
-            return status_code[2];
+            returnMessage(soc, (status_code[1]));
         }
     }
     DIR *dp;
     int i = getCount(path);
-<<<<<<< HEAD
-    std::string filePath = path + "/" + std::to_string(i) + "_" + message[3];
-    std::ofstream MailFile(filePath);
-    MailFile << "SENDER: " << message[1] << std::endl;
-    MailFile << "SUBJECT: " << message[3] << std::endl;
-    MailFile << "CONTENT: " << message[4] << std::endl;
-=======
-    std::string filePath = path + "/" + message[3] + "_" + std::to_string(i) + ".txt";
+    std::string filePath = path + "/" + std::to_string(i) + "_" +   message[3] + ".txt";
     std::ofstream MailFile(filePath);
     MailFile << "SENDER: " << message[1] << std::endl;
     MailFile << "SUBJECT: " << message[3] << std::endl;
@@ -104,22 +82,10 @@ char *sendHandler(std::vector<std::string> message, char *dir)
         MailFile << message[n] << std::endl;
         n++;
     }
->>>>>>> ec6f38af7bbbfb294cb6ed25bddd8fa5073bc915
     MailFile.close();
-    return status_code[0];
+    returnMessage(soc, (status_code[0]));
 }
 
-<<<<<<< HEAD
-void listHandler(std::vector<std::string> message, char *dir)
-
-{
-    DIR *dp = opendir("./");
-
-    if (dp != NULL)
-    {
-        return;
-    }
-=======
 void readHandler (std::vector<std::string> message, char *dir, int soc){
     //Identify file
     std::string path = dir;
@@ -156,27 +122,22 @@ void listHandler(std::vector<std::string> message, char *dir, int soc) {
     //define path
     std::string path = dir;
     path += '/' + message[1];
+    int count = 0;
     DIR *dp;
     std::string out = "";
 
-    std::string count = "";
     if (dp = opendir(path.c_str()))
     {
         struct dirent *ent;
-
-        //get counter
-        std::ifstream is_counter;
-        is_counter.open(path + "/count");
-        is_counter >> count;
-        is_counter.close();
-        count += '\n';
 
         //get subjects
         while ((ent = readdir(dp)) != NULL)
         {
             std::string temp = ent->d_name;
-            if (temp != "count" && temp.size() > 3)
+            if (temp != "index" && temp != ".." && temp != ".")
             {
+                count++;
+                temp.replace(temp.end()-4,temp.end(), "");
                 out += temp + '\n';
             }
         }
@@ -186,7 +147,7 @@ void listHandler(std::vector<std::string> message, char *dir, int soc) {
     {
         out = "0";
     }
-    out.insert(0, count);
+    out.insert(0, std::to_string(count) + '\n');
     returnMessage(soc, (char *)out.c_str());
 }
 
@@ -203,7 +164,6 @@ void deleteHandler(std::vector<std::string> message, char *dir, int soc){
                 std::string fileName = path + "/" + ent->d_name;
                 if (remove(fileName.c_str()) == 0){
                     out = status_code[0];
-                    //get count down
                     break;
                 }
             }
@@ -211,16 +171,11 @@ void deleteHandler(std::vector<std::string> message, char *dir, int soc){
         closedir(dp);
     }
     returnMessage(soc, (char *)out.c_str());
->>>>>>> ec6f38af7bbbfb294cb6ed25bddd8fa5073bc915
 }
 
 void mailHandler(char *input, int clientSocket, char *directory)
 {
-<<<<<<< HEAD
-    printf("Message received: %s", input);
-=======
     // printf("Message received: %s", input);
->>>>>>> ec6f38af7bbbfb294cb6ed25bddd8fa5073bc915
 
     std::vector<std::string> messageParsed;
     std::string parsed;
@@ -234,21 +189,10 @@ void mailHandler(char *input, int clientSocket, char *directory)
     // TODO: ROUTING HERE
     if (messageParsed[0] == "SEND")
     {
-        returnMessage(clientSocket, sendHandler(messageParsed, directory));
+        sendHandler(messageParsed, directory, clientSocket);
     }
     else if (messageParsed[0] == "LIST")
     {
-<<<<<<< HEAD
-        defaultMethod(messageParsed);
-    }
-    else if (messageParsed[0] == "READ")
-    {
-        defaultMethod(messageParsed);
-    }
-    else if (messageParsed[0] == "DEL")
-    {
-        defaultMethod(messageParsed);
-=======
         listHandler(messageParsed, directory, clientSocket);
     }
     else if (messageParsed[0] == "READ")
@@ -258,14 +202,10 @@ void mailHandler(char *input, int clientSocket, char *directory)
     else if (messageParsed[0] == "DEL")
     {
         deleteHandler(messageParsed, directory, clientSocket);
->>>>>>> ec6f38af7bbbfb294cb6ed25bddd8fa5073bc915
     }
     else
     {
         returnMessage(clientSocket, status_code[2]);
     }
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> ec6f38af7bbbfb294cb6ed25bddd8fa5073bc915
--- Server/src/statusCodes.cpp
+++ Server/src/statusCodes.cpp
@@ -1,10 +1,6 @@
 #define SERVER_STATUS_CODES
 
-<<<<<<< HEAD
-const char status_code[][50] = {
-=======
 char status_code[][50] = {
->>>>>>> ec6f38af7bbbfb294cb6ed25bddd8fa5073bc915
-    {"200 HTTP OK"},
-    {"404 NOT FOUND"},
-    {"500 Internal Server Error"}};
+    {"200 HTTP OK\n"},
+    {"404 NOT FOUND\n"},
+    {"500 Internal Server Error\n"}};
